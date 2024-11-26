beb4e83ea59f163a7d454ccd5ae3035598d52d69
Thuy Nguyen <nguyenthethuy.qnam@gmail.com>
2024-11-26 10:02:18 +0700

    feat: update football home page

--- views/football-manager.php
+++ views/football-manager.php
@@ -9,61 +9,92 @@ $commonController = new CommonController();
 ob_start();
 ?>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <?php includeFileWithVariables('components/football-player-topbar.php'); ?>
-                </div>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <?php includeFileWithVariables('components/football-player-topbar.php'); ?>
             </div>
         </div>
-        <div class="col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    <div class="card-title mb-0">
-                        Popular Players
-                    </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title mb-0">
+                    Popular Players
                 </div>
-                <div class="card-body">
-                    <div class="list-group">
-                        <?php foreach ($popular_players as $player) { ?>
-                            <div class="list-group-item list-group-item-action">
-                                <div class="float-end">
-                                    <div class="d-flex flex-column align-items-end">
-                                        <?= formatCurrency($player['market_value']) ?>
-                                        <button class="btn btn-sm btn-outline-success mt-1" style="width: 40px">Buy
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <div class="flex-shrink-0">
-                                        <img src="assets/images/users/avatar-1.jpg" alt=""
-                                             class="avatar-sm rounded-circle"/>
-                                    </div>
-                                    <div class="flex-grow-1 ms-3">
-                                        <h5 class="list-title fs-15 mb-1"><?= $player['name'] ?></h5>
-                                        <p class="list-text mb-0 fs-12"><?= $player['best_position'] ?>
-                                            | <?= $player['abilities']['current_ability'] ?>
-                                            | <?= implode(", ", $player['playable_positions']) ?></p>
-                                    </div>
+            </div>
+            <div class="card-body">
+                <div class="list-group">
+                    <?php foreach ($popular_players as $player) { ?>
+                        <div class="list-group-item list-group-item-action">
+                            <div class="float-end">
+                                <div class="d-flex flex-column align-items-end">
+                                    <?= formatCurrency($player['market_value']) ?>
+                                    <button class="btn btn-sm btn-outline-success mt-1" style="width: 40px">Buy
+                                    </button>
                                 </div>
                             </div>
-                        <?php } ?>
-                    </div>
+                            <div class="d-flex align-items-center">
+                                <div class="flex-shrink-0">
+                                    <img src="assets/images/users/avatar-1.jpg" alt=""
+                                        class="avatar-sm rounded-circle" />
+                                </div>
+                                <div class="flex-grow-1 ms-3">
+                                    <h5 class="list-title fs-15 mb-1"><?= $player['name'] ?></h5>
+                                    <p class="list-text mb-0 fs-12"><?= $player['best_position'] ?>
+                                        | <?= $player['abilities']['current_ability'] ?>
+                                        | <?= implode(", ", $player['playable_positions']) ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4"></div>
-        <div class="col-lg-4">
-            <div class="card card-height-100">
-                <div class="card-header">
-                    <div class="card-title mb-0">
-                        Team Ranking
+    </div>
+    <div class="col-lg-4">
+        <div class="card card-body text-center">
+            <div class="row py-3 mb-2">
+                <div class="col-6">
+                    <div class="avatar-sm mx-auto mb-3">
+                        <div class="avatar-title bg-success-subtle text-success fs-17 rounded">
+                            <i class="ri-add-line"></i>
+                        </div>
                     </div>
+                    <h4 class="card-title">Crimson Falcons</h4>
                 </div>
-                <div class="card-body p-0">
-                    <table class="table align-middle table-nowrap mb-0">
-                        <thead class="table-light">
+                <div class="col-6">
+                    <div class="avatar-sm mx-auto mb-3">
+                        <div class="avatar-title bg-success-subtle text-success fs-17 rounded">
+                            <i class="ri-add-line"></i>
+                        </div>
+                    </div>
+                    <h4 class="card-title">Azure Knights</h4>
+                </div>
+            </div>
+            <button class="btn btn-success">Game On</button>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0 text-center">Challenge Match</h4>
+            </div>
+            <div class="card-body text-center">
+                <p class="card-text py-2 mb-3">You must pay <b>$2,000</b> to participate in the challenge match. If you win, you'll take home <b>$2,800</b>!</p>
+                <button class="btn btn-soft-primary">Challenge Now</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="card card-height-100">
+            <div class="card-header">
+                <div class="card-title mb-0">
+                    Team Ranking
+                </div>
+            </div>
+            <div class="card-body p-0">
+                <table class="table align-middle table-nowrap mb-0">
+                    <thead class="table-light">
                         <tr>
                             <th scope="col" class="text-center" style="width: 52px">Rank</th>
                             <th scope="col">Team</th>
@@ -71,11 +102,11 @@ ob_start();
                             <th scope="col" class="text-center" style="width: 52px">Draw</th>
                             <th scope="col" class="text-center" style="width: 52px">Lose</th>
                         </tr>
-                        </thead>
-                    </table>
-                    <div data-simplebar style="max-height: 560px;">
-                        <table class="table align-middle table-nowrap mb-0">
-                            <tbody>
+                    </thead>
+                </table>
+                <div data-simplebar style="max-height: 560px;">
+                    <table class="table align-middle table-nowrap mb-0">
+                        <tbody>
                             <?php foreach (DEFAULT_FOOTBALL_TEAM as $index => $team) { ?>
                                 <tr>
                                     <td class="text-center" style="width: 52px"><?= $index + 1 ?></td>
@@ -96,14 +127,14 @@ ob_start();
                                     <td class="text-center" style="width: 52px">1</td>
                                 </tr>
                             <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-        <!--end col-->
     </div>
+    <!--end col-->
+</div>
 
 <?php
 $pageContent = ob_get_clean();
