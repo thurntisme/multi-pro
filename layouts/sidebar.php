e4abd36076e2cb6b8aa2867257697c91c38d0a2b
Thuy Nguyen <nguyenthethuy.qnam@gmail.com>
2024-12-24 22:22:12 +0700

    add stack: php, mysql, english

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -21,7 +21,7 @@
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
-            id="vertical-hover">
+                id="vertical-hover">
             <i class="ri-record-circle-line"></i>
         </button>
     </div>
@@ -50,89 +50,108 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("daily-checklist") ?>"
-                        href="<?= home_url("app/daily-checklist") ?>">
+                       href="<?= home_url("app/daily-checklist") ?>">
                         <i class="ri-calendar-check-line"></i> <span data-key="t-daily-checklist">Daily Checklist</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("finance") ?>" href="<?= home_url("app/finance") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("finance") ?>"
+                       href="<?= home_url("app/finance") ?>">
                         <i class="ri-wallet-3-line"></i> <span data-key="t-finance">Finance</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("subscription") ?>"
-                        href="<?= home_url("app/subscription") ?>">
+                       href="<?= home_url("app/subscription") ?>">
                         <i class="ri-calendar-check-line"></i> <span data-key="t-subscription">Subscription</span>
                     </a>
                 </li>
                 <li class="menu-title"><span data-key="t-tech-stack">Tech Stack</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("wordpress") ?>" href="<?= home_url("app/wordpress") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("wordpress") ?>"
+                       href="<?= home_url("app/wordpress") ?>">
                         <i class="mdi mdi-wordpress"></i> <span data-key="t-wordpress">Wordpress</span>
                     </a>
-                    <a class="nav-link menu-link <?= activeClassName("reactjs") ?>" href="<?= home_url("app/reactjs") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("reactjs") ?>"
+                       href="<?= home_url("app/reactjs") ?>">
                         <i class="mdi mdi-react"></i> <span>ReactJS</span>
                     </a>
-                    <a class="nav-link menu-link <?= activeClassName("salesforce") ?>" href="<?= home_url("app/salesforce") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("salesforce") ?>"
+                       href="<?= home_url("app/salesforce") ?>">
                         <i class="mdi mdi-salesforce"></i> <span>Salesforce</span>
+                    </a>
+                    <a class="nav-link menu-link <?= activeClassName("php") ?>"
+                       href="<?= home_url("app/php") ?>">
+                        <i class="mdi mdi-language-php"></i> <span>PHP</span>
+                    </a>
+                    <a class="nav-link menu-link <?= activeClassName("mysql") ?>"
+                       href="<?= home_url("app/mysql") ?>">
+                        <i class="mdi mdi-database-outline"></i> <span>MySql</span>
+                    </a>
+                    <a class="nav-link menu-link <?= activeClassName("english") ?>"
+                       href="<?= home_url("app/english") ?>">
+                        <i class="mdi mdi-google-translate"></i> <span>English</span>
                     </a>
                 </li>
                 <li class="menu-title"><span data-key="t-app">App</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("bookmark") ?>" href="<?= home_url("app/bookmark") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("bookmark") ?>"
+                       href="<?= home_url("app/bookmark") ?>">
                         <i class="ri-bookmark-line"></i> <span data-key="t-bookmark">Bookmark</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("short-link") ?>"
-                        href="<?= home_url("app/short-link") ?>">
+                       href="<?= home_url("app/short-link") ?>">
                         <i class="ri-link-m"></i> <span data-key="t-short-link">Short Link</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("generate-qr") ?>"
-                        href="<?= home_url("app/generate-qr") ?>">
+                       href="<?= home_url("app/generate-qr") ?>">
                         <i class="ri-qr-code-line"></i> <span data-key="t-generate-qr">Generate QR</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("ai-chat") ?>" href="<?= home_url("app/ai-chat") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("ai-chat") ?>"
+                       href="<?= home_url("app/ai-chat") ?>">
                         <i class="ri-chat-3-line"></i> <span data-key="t-ai-chat">AI Chat</span>
                     </a>
                 </li>
                 <li class="menu-title"><span data-key="t-work">Work</span></li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("projects") ?>" href="<?= home_url("app/projects") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("projects") ?>"
+                       href="<?= home_url("app/projects") ?>">
                         <i class="ri-folder-5-line"></i> <span data-key="t-projects">Projects</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("report-working") ?>"
-                        href="<?= home_url("app/report-working") ?>">
+                       href="<?= home_url("app/report-working") ?>">
                         <i class="ri-clipboard-line"></i> <span data-key="t-report-working">Report Working</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("maintain-web") ?>"
-                        href="<?= home_url("app/maintain-web") ?>">
+                       href="<?= home_url("app/maintain-web") ?>">
                         <i class="ri-global-line"></i> <span data-key="t-maintain-web">Maintain Web</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("seo-checklist") ?>"
-                        href="<?= home_url("app/seo-checklist") ?>">
+                       href="<?= home_url("app/seo-checklist") ?>">
                         <i class="ri-search-line"></i> <span data-key="t-seo-checklist">SEO Checklist</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("web-dev-checklist") ?>"
-                        href="<?= home_url("app/web-dev-checklist") ?>">
+                       href="<?= home_url("app/web-dev-checklist") ?>">
                         <i class="ri-tools-line"></i> <span data-key="t-web-dev-checklist">Web Dev Checklist</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("web-secure-checklist") ?>"
-                        href="<?= home_url("app/web-secure-checklist") ?>">
+                       href="<?= home_url("app/web-secure-checklist") ?>">
                         <i class="ri-shield-line"></i> <span data-key="t-web-secure-checklist">Web Secure
                             Checklist</span>
                     </a>
@@ -175,20 +194,15 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("website") ?>" href="<?= home_url("app/website") ?>">
+                    <a class="nav-link menu-link <?= activeClassName("website") ?>"
+                       href="<?= home_url("app/website") ?>">
                         <i class="ri-links-line"></i> <span data-key="t-website">Website</span>
-                    </a>
-                </li>
-                <li class="menu-title"><span data-key="t-network">English</span></li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link <?= activeClassName("grammar") ?>" href="<?= home_url("app/grammar") ?>">
-                        <i class="ri-booklet-line"></i> <span data-key="t-grammar">Grammar</span>
                     </a>
                 </li>
                 <li class="menu-title"><span data-key="t-network">Game</span></li>
                 <li class="nav-item">
                     <a class="nav-link menu-link <?= activeClassName("football-manager") ?>"
-                        href="<?= home_url("app/football-manager") ?>">
+                       href="<?= home_url("app/football-manager") ?>">
                         <i class="ri-football-line"></i> <span data-key="t-football-manager">Football Manager</span>
                     </a>
                 </li>
