acdfd63551ad31c20642a5907c6e1d390ddb76dc
Thuy Nguyen <nguyenthethuy.qnam@gmail.com>
2024-12-25 17:31:30 +0700

    update course layout

--- views/course-adjust.php
+++ views/course-adjust.php
@@ -19,7 +19,7 @@ if (!empty($modify_type)) {
     }
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['action_name'])) {
-            if ($_POST['action_name'] === 'delete_record' && isset($_GET['id'])) {
+            if ($_POST['action_name'] === 'delete_record') {
                 $courseController->deleteCourse();
             }
         } else {
@@ -35,14 +35,12 @@ if (!empty($modify_type)) {
 
 ob_start();
 ?>
-<form method="POST" action="<?= $_SERVER['REQUEST_URI'] ?>" id="course">
-    <div class="row">
-        <div class="col-lg-8">
-
-            <?php
-            include_once DIR . '/components/alert.php';
-            ?>
-
+<div class="row">
+    <div class="col-xl-8 col-md-10 offset-xl-2 offset-md-1">
+        <?php
+        includeFileWithVariables('components/single-button-group.php', array("slug" => "course", "post_id" => $postData['id'], 'modify_type' => $modify_type));
+        ?>
+        <form method="POST" action="<?= $_SERVER['REQUEST_URI'] ?>" id="course">
             <div class="card">
                 <div class="card-body">
                     <div class="mb-3">
@@ -50,7 +48,7 @@ ob_start();
                         <input type="text" class="form-control" id="course-title-input" name="title"
                             placeholder="Enter title" value="<?= $postData['title'] ?? '' ?>">
                         <?php if (!empty($post_id)) { ?>
-                        <input type="hidden" name="course_id" value="<?= $post_id ?>">
+                            <input type="hidden" name="course_id" value="<?= $post_id ?>">
                         <?php } ?>
                     </div>
 
@@ -67,171 +65,32 @@ ob_start();
                             value="<?= $postData['url'] ?? '' ?>">
                     </div>
 
-                    <div class="row">
-                        <div class="col-lg-4">
-                            <div class="mb-3 mb-lg-0">
-                                <label for="choices-priority-input" class="form-label">Priority</label>
-                                <select class="form-select" data-choices data-choices-search-false
-                                    id="choices-priority-input">
-                                    <option value="High" selected>High</option>
-                                    <option value="Medium">Medium</option>
-                                    <option value="Low">Low</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="mb-3 mb-lg-0">
-                                <label for="choices-status-input" class="form-label">Status</label>
-                                <select class="form-select" data-choices data-choices-search-false
-                                    id="choices-status-input" name="status">
-                                    <option value="not_started" selected>Not Started</option>
-                                    <option value="inprogress">Inprogress</option>
-                                    <option value="completed">Completed</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div>
-                                <label for="datepicker-deadline-input" class="form-label">Deadline</label>
-                                <input type="text" class="form-control" id="datepicker-deadline-input"
-                                    placeholder="Enter due date" data-provider="flatpickr">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
-            <!-- end card -->
-            <div class="text-center mb-4">
-                <a href="<?= home_url('course') ?>" class="btn btn-light w-sm">Back</a>
-                <button type="submit"
-                    class="btn btn-success w-sm"><?= $modify_type === "new" ? "Create" : "Save" ?></button>
-            </div>
-        </div>
-        <!-- end col -->
-        <div class="col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Action</h5>
-                </div>
-                <div class="card-body">
-                    <a href="<?= home_url('course') ?>" class="btn btn-light w-sm">Back</a>
-                    <a href="<?= home_url('course/detail?id=' . $postData['id']) ?>"
-                        class="btn btn-info w-sm mx-2">View</a>
-                    <?php if (!empty($post_id)) { ?>
-                    <button type="button" class="btn btn-danger w-sm" data-bs-toggle="modal"
-                        data-bs-target="#deleteRecordModal">Delete</button>
-                    <?php } ?>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Privacy</h5>
-                </div>
-                <div class="card-body">
-                    <div>
-                        <label for="choices-privacy-status-input" class="form-label">Status</label>
-                        <select class="form-select" data-choices data-choices-search-false
-                            id="choices-privacy-status-input">
-                            <option value="Private" selected>Private</option>
-                            <option value="Team">Team</option>
-                            <option value="Public">Public</option>
-                        </select>
-                    </div>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Tags</h5>
-                </div>
-                <div class="card-body">
                     <div class="mb-3">
-                        <label for="choices-categories-input" class="form-label">Categories</label>
+                        <label for="choices-status-input" class="form-label">Status</label>
                         <select class="form-select" data-choices data-choices-search-false
-                            id="choices-categories-input">
-                            <option value="Designing" selected>Designing</option>
-                            <option value="Development">Development</option>
+                            id="choices-status-input" name="status">
+                            <option value="not_started" selected>Not Started</option>
+                            <option value="inprogress">Inprogress</option>
+                            <option value="completed">Completed</option>
                         </select>
                     </div>
 
-                    <div>
+                    <div class="mb-3">
                         <label for="choices-text-input" class="form-label">Tags</label>
-                        <input class="form-control" id="choices-text-input" data-choices
+                        <input class="form-control" id="choices-text-input" data-choices data-choices-removeItem
                             data-choices-limit="Required Limit" placeholder="Enter Skills" type="text" name="tags"
                             value="<?= $postData['tags'] ?? '' ?>" />
                     </div>
                 </div>
                 <!-- end card body -->
             </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Members</h5>
-                </div>
-                <div class="card-body">
-                    <div class="mb-3">
-                        <label for="choices-lead-input" class="form-label">Team Lead</label>
-                        <select class="form-select" data-choices data-choices-search-false id="choices-lead-input">
-                            <option value="Brent Gonzalez" selected>Brent Gonzalez</option>
-                            <option value="Darline Williams">Darline Williams</option>
-                            <option value="Sylvia Wright">Sylvia Wright</option>
-                            <option value="Ellen Smith">Ellen Smith</option>
-                            <option value="Jeffrey Salazar">Jeffrey Salazar</option>
-                            <option value="Mark Williams">Mark Williams</option>
-                        </select>
-                    </div>
-
-                    <div>
-                        <label class="form-label">Team Members</label>
-                        <div class="avatar-group">
-                            <a href="javascript: void(0);" class="avatar-group-item" data-bs-toggle="tooltip"
-                                data-bs-trigger="hover" data-bs-placement="top" title="Brent Gonzalez">
-                                <div class="avatar-xs">
-                                    <img src="assets/images/users/avatar-3.jpg" alt="" class="rounded-circle img-fluid">
-                                </div>
-                            </a>
-                            <a href="javascript: void(0);" class="avatar-group-item" data-bs-toggle="tooltip"
-                                data-bs-trigger="hover" data-bs-placement="top" title="Sylvia Wright">
-                                <div class="avatar-xs">
-                                    <div class="avatar-title rounded-circle bg-secondary">
-                                        S
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="javascript: void(0);" class="avatar-group-item" data-bs-toggle="tooltip"
-                                data-bs-trigger="hover" data-bs-placement="top" title="Ellen Smith">
-                                <div class="avatar-xs">
-                                    <img src="assets/images/users/avatar-4.jpg" alt="" class="rounded-circle img-fluid">
-                                </div>
-                            </a>
-                            <a href="javascript: void(0);" class="avatar-group-item" data-bs-toggle="tooltip"
-                                data-bs-trigger="hover" data-bs-placement="top" title="Add Members">
-                                <div class="avatar-xs" data-bs-toggle="modal" data-bs-target="#inviteMembersModal">
-                                    <div
-                                        class="avatar-title fs-16 rounded-circle bg-light border-dashed border text-primary">
-                                        +
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <!-- end card body -->
+            <div class="text-center mb-4">
+                <button type="submit"
+                    class="btn btn-success w-sm"><?= $modify_type === "new" ? "Create" : "Save" ?></button>
             </div>
-            <!-- end card -->
-        </div>
-        <!-- end col -->
+        </form>
     </div>
-</form>
+</div>
 
 <?php
 include_once DIR . '/components/modal-delete.php';
--- views/course.php
+++ views/course.php
@@ -19,19 +19,19 @@ include_once DIR . '/components/alert.php';
             <h5 class="card-title mb-0 flex-grow-1">All Courses</h5>
             <div class="flex-shrink-0">
                 <div class="d-flex flex-wrap gap-2">
-                    <a class="btn btn-danger add-btn" href="<?= home_url('course/new') ?>"><i
+                    <a class="btn btn-soft-success add-btn" href="<?= home_url('app/course/new') ?>"><i
                             class="ri-add-line align-bottom me-1"></i> Create Course</a>
                 </div>
             </div>
         </div>
     </div>
     <div class="card-body border border-dashed border-end-0 border-start-0">
-        <form method="get" action="<?= home_url('course') ?>">
+        <form method="get" action="<?= home_url('app/course') ?>">
             <div class="row g-3">
                 <div class="col-xxl-5 col-sm-12">
                     <div class="search-box">
                         <input type="text" name="s" class="form-control search bg-light border-light"
-                            placeholder="Search for courses or something...">
+                            placeholder="Search for courses or something..." value="<?= $_GET['s'] ?? '' ?>">
                         <i class="ri-search-line search-icon"></i>
                     </div>
                 </div>
@@ -43,23 +43,13 @@ include_once DIR . '/components/alert.php';
                         placeholder="Select date range">
                 </div>
                 <!--end col-->
-
-                <div class="col-xxl-3 col-sm-4">
-                    <div class="input-light">
-                        <select class="form-control" data-choices data-choices-search-false
-                            name="choices-single-default" id="idStatus">
-                            <option value="" selected>All</option>
-                            <option value="monthly">Monthly</option>
-                            <option value="yearly">Yearly</option>
-                        </select>
-                    </div>
-                </div>
-                <!--end col-->
-                <div class="col-xxl-1 col-sm-4">
-                    <button type="submit" class="btn btn-primary w-100"> <i
+                <div class="col-xxl-2 col-sm-4 d-flex">
+                    <button type="submit" class="btn btn-primary"><i
                             class="ri-equalizer-fill me-1 align-bottom"></i>
                         Filters
                     </button>
+                    <a href="<?= home_url("app/todo") ?>" class="btn btn-danger ms-1"><i
+                            class="ri-delete-bin-2-fill me-1 align-bottom"></i>Reset</a>
                 </div>
                 <!--end col-->
             </div>
@@ -73,8 +63,8 @@ include_once DIR . '/components/alert.php';
                 <thead class="table-light text-muted">
                     <tr>
                         <th>Title</th>
-                        <th>Tags</th>
-                        <th>Status</th>
+                        <th class="text-center">Tags</th>
+                        <th class="text-center">Status</th>
                         <th class="sort text-end pe-5">Created Date</th>
                         <th class="sort text-end pe-5">Updated Date</th>
                     </tr>
@@ -82,41 +72,34 @@ include_once DIR . '/components/alert.php';
                 <tbody class="list form-check-all">
                     <?php if (count($list['list']) > 0) {
                         foreach ($list['list'] as $item) { ?>
-                    <tr>
-                        <td>
-                            <div class="d-flex">
-                                <div class="flex-grow-1"><?= truncateString($item['title'], 50) ?></div>
-                                <div class="flex-shrink-0 ms-4">
-                                    <ul class="list-inline tasks-list-menu mb-0">
-                                        <li class="list-inline-item"><a
-                                                href="<?= home_url('course/detail?id=' . $item['id']) ?>"><i
-                                                    class="ri-eye-fill align-bottom me-2 text-muted"></i></a></li>
-                                        <li class="list-inline-item"><a class="edit-item-btn"
-                                                href="<?= home_url('course/edit?id=' . $item['id']) ?>"><i
-                                                    class="ri-pencil-fill align-bottom me-2 text-muted"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </td>
-                        <td><?= $item['tags'] ?></td>
-                        <td><?= $item['status'] ?></td>
-                        <td class="text-end pe-5"><?= $commonController->convertDate($item['created_at']) ?></td>
-                        <td class="text-end pe-5"><?= $commonController->convertDate($item['updated_at']) ?></td>
-                    </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex align-items-baseline">
+                                        <a class="text-black"
+                                            href="<?= home_url('app/course/detail?id=' . $item['id']) ?>"><?= truncateString($item['title'], 50) ?></a>
+                                        <ul class="list-inline tasks-list-menu mb-0 ms-3">
+                                            <li class="list-inline-item m-0"><a
+                                                    class="edit-item-btn btn btn-link btn-sm"
+                                                    href="<?= home_url('app/course/detail?id=' . $item['id']) ?>"><i
+                                                        class="ri-eye-fill align-bottom text-muted"></i></a>
+                                            </li>
+                                            <li class="list-inline-item m-0"><a
+                                                    class="edit-item-btn btn btn-link btn-sm"
+                                                    href="<?= home_url('app/course/edit?id=' . $item['id']) ?>"><i
+                                                        class="ri-pencil-fill align-bottom text-muted"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </td>
+                                <td class="text-center"><?= $item['tags'] ?></td>
+                                <td class="text-center"><?= renderStatusBadge($item['status']) ?></td>
+                                <td class="text-end pe-5"><?= $commonController->convertDate($item['created_at']) ?></td>
+                                <td class="text-end pe-5"><?= $commonController->convertDate($item['updated_at']) ?></td>
+                            </tr>
                     <?php }
                     } ?>
                 </tbody>
             </table>
-            <!--end table-->
-            <div class="noresult" style="display: none">
-                <div class="text-center">
-                    <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
-                        colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px"></lord-icon>
-                    <h5 class="mt-2">Sorry! No Result Found</h5>
-                    <p class="text-muted mb-0">Weve searched more than 200k+ courses We did not find any
-                        courses for you search.</p>
-                </div>
-            </div>
         </div>
         <?php
         includeFileWithVariables('components/pagination.php', array("count" => $list['count']));
