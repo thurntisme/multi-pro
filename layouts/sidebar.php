d7732479e5bde33d6171fb91d9492fdc95030458
Thuy Nguyen <nguyenthethuy.qnam@gmail.com>
2025-01-07 11:32:42 +0700

    update website logo

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -271,19 +271,19 @@ $menu_items = [
         <!-- Dark Logo-->
         <a href="<?= home_url('app/') ?>" class="logo logo-dark">
             <span class="logo-sm">
-                <img src="<?= home_url("assets/images/logo-sm.png") ?>" alt="" height="22">
+                <img src="<?= home_url("assets/images/logo-sm.png") ?>" alt="" width="32" height="32">
             </span>
             <span class="logo-lg">
-                <img src="<?= home_url("assets/images/logo-dark.png") ?>" alt="" height="17">
+                <img src="<?= home_url("assets/images/dark-logo.png") ?>" alt="" height="29">
             </span>
         </a>
         <!-- Light Logo-->
         <a href="<?= home_url('app/') ?>" class="logo logo-light">
             <span class="logo-sm">
-                <img src="<?= home_url("assets/images/logo-sm.png") ?>" alt="" height="22">
+                <img src="<?= home_url("assets/images/logo-sm.png") ?>" alt="" width="32" height="32">
             </span>
             <span class="logo-lg">
-                <img src="<?= home_url("assets/images/logo-light.png") ?>" alt="" height="17">
+                <img src="<?= home_url("assets/images/light-logo.png") ?>" alt="" height="29">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
--- views/landing.php
+++ views/landing.php
@@ -34,8 +34,8 @@
     <nav class="navbar navbar-expand-lg navbar-landing fixed-top" id="navbar">
       <div class="container">
         <a class="navbar-brand" href="<?= home_url('') ?>">
-          <img src="assets/images/logo-dark.png" class="card-logo card-logo-dark" alt="logo dark" height="17">
-          <img src="assets/images/logo-light.png" class="card-logo card-logo-light" alt="logo light" height="17">
+          <img src="<?= home_url("assets/images/dark-logo.png") ?>" class="card-logo card-logo-dark" alt="logo dark" height="29">
+          <img src="<?= home_url("assets/images/light-logo.png") ?>" class="card-logo card-logo-light" alt="logo light" height="29">
         </a>
         <button class="navbar-toggler py-0 fs-20 text-body" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <i class="mdi mdi-menu"></i>
