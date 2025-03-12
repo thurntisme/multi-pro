12dc50f18e1522e13c485ad44ee1c70c2a6b0545
Thuy Nguyen <nguyenthethuy.qnam@gmail.com>
2025-03-12 21:56:54 +0700

    implement api for ai_service

--- python-ai/app/main.py
+++ python-ai/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers import ai_router
 
 app = FastAPI()
-app.include_router(ai_router.router)
+# app.include_router(ai_router.router)
 
 @app.get("/")
 def home():
