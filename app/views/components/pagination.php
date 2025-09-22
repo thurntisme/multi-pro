17e7c535c36f23e0bfd35e2331de69ee1aed4861
Thuy Nguyen <nguyenthethuy.qnam@gmail.com>
2025-09-22 18:13:29 +0700

    refactor: move components to views directory

--- app/views/components/pagination.php
+++ app/views/components/pagination.php
@@ -3,7 +3,7 @@
 if ($count > 0) {
     $itemsPerPage = $perPage ?? 10; // Number of results per page
     $totalItems = $count; // Total number of items
-    $currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1; // Current page from the query string
+    $currentPage = isset($_GET['page']) ? (int) $_GET['page'] : 1; // Current page from the query string
     $totalPages = ceil($totalItems / $itemsPerPage); // Calculate total pages
     // Calculate the starting and ending entry numbers
     $startEntry = ($currentPage - 1) * $itemsPerPage + 1;
@@ -14,7 +14,7 @@ if ($count > 0) {
     if ($currentPage > 1) {
         $prevPage = $currentPage - 1;
         $prevLink = '<li class="page-item">
-                    <a href="' . generatePageUrl(['page' => $prevPage]) . '" class="page-link">Previous</a>
+                    <a href="' . App\Helpers\NetworkHelper::generatePageUrl(['page' => $prevPage]) . '" class="page-link">Previous</a>
                 </li>';
     } else {
         $prevLink = '<li class="page-item disabled">
@@ -27,7 +27,7 @@ if ($count > 0) {
     if ($currentPage < $totalPages) {
         $nextPage = $currentPage + 1;
         $nextLink = '<li class="page-item">
-                    <a href="' . generatePageUrl(['page' => $nextPage]) . '" class="page-link">Next</a>
+                    <a href="' . App\Helpers\NetworkHelper::generatePageUrl(['page' => $nextPage]) . '" class="page-link">Next</a>
                 </li>';
     } else {
         $nextLink = '<li class="page-item disabled">
@@ -44,7 +44,7 @@ if ($count > 0) {
         // Add the first page link if needed
         if ($displayFirstLast && $currentPage > ($adjacentPages + 1)) {
             $pageLinks .= '<li class="page-item">
-                <a href="' . generatePageUrl(['page' => 1]) . '" class="page-link">1</a>
+                <a href="' . App\Helpers\NetworkHelper::generatePageUrl(['page' => 1]) . '" class="page-link">1</a>
             </li>';
             $pageLinks .= '<li class="page-item disabled"><a href="#" class="page-link">...</a></li>';
         }
@@ -57,7 +57,7 @@ if ($count > 0) {
                 </li>';
             } else {
                 $pageLinks .= '<li class="page-item">
-                    <a href="' . generatePageUrl(['page' => $i]) . '" class="page-link">' . $i . '</a>
+                    <a href="' . App\Helpers\NetworkHelper::generatePageUrl(['page' => $i]) . '" class="page-link">' . $i . '</a>
                 </li>';
             }
         }
@@ -66,22 +66,24 @@ if ($count > 0) {
         if ($displayFirstLast && $currentPage < ($totalPages - $adjacentPages)) {
             $pageLinks .= '<li class="page-item disabled"><a href="#" class="page-link">...</a></li>';
             $pageLinks .= '<li class="page-item">
-                <a href="' . generatePageUrl(['page' => $totalPages]) . '" class="page-link">' . $totalPages . '</a>
+                <a href="' . App\Helpers\NetworkHelper::generatePageUrl(['page' => $totalPages]) . '" class="page-link">' . $totalPages . '</a>
             </li>';
         }
     }
-
-    echo '<div class="row g-0 text-center text-sm-start align-items-center mb-4">
+    ?>
+    <div class="row g-0 text-center text-sm-start align-items-center mb-4">
         <div class="col-sm-6">
             <div>
-                <p class="mb-sm-0 text-muted">Showing <span class="fw-semibold">' . $startEntry . '</span> to <span class="fw-semibold">' . $endEntry . '</span> of <span class="fw-semibold text-decoration-underline">' . $totalItems . '</span> entries</p>
+                <p class="mb-sm-0 text-muted">Showing <span class="fw-semibold"><?= $startEntry ?></span> to <span
+                        class="fw-semibold"><?= $endEntry ?></span> of <span
+                        class="fw-semibold text-decoration-underline"><?= $totalItems ?></span> entries</p>
             </div>
         </div>
         <!-- end col -->
         <div class="col-sm-6">
             <ul class="pagination pagination-separated justify-content-center justify-content-sm-end mb-sm-0">
-                ' . $prevLink . $pageLinks . $nextLink . '
+                <?= $prevLink . $pageLinks . $nextLink ?>
             </ul>
         </div><!-- end col -->
-</div><!-- end row -->';
-}
+    </div><!-- end row -->
+<?php } ?>
